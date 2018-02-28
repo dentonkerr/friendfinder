ed6c28973a8ced73add41db7a6b73733f5af4a4a
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2018-02-28 17:47:18 -0500

    adding apiRoute coding, friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,71 @@ var friendsArray = [
             3,
             1
         ]
-    }
+    },
+    {
+        name: "Nick",
+        photo: "https://usatftw.files.wordpress.com/2017/11/usp_ncaa_football__mercer_at_alabama_95420157-e1511653563499.jpg?w=1000&h=600&crop=1",
+        scores: [
+            3,
+            1,
+            5,
+            4,
+            2,
+            1,
+            5,
+            4,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Dabo",
+        photo: "https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Ffansided.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F08%2F833715546-cfp-national-championship.jpg.jpg&c=sc&w=850&h=560",
+        scores: [
+            5,
+            2,
+            1,
+            4,
+            2,
+            3,
+            5,
+            2,
+            1,
+            3
+        ]
+    },
+    {
+        name: "Urban",
+        photo: "https://cdn.vox-cdn.com/thumbor/BN6WJufdpfLh3doEyN1jwr8nV8o=/0x0:3240x2160/1200x800/filters:focal(1361x821:1879x1339)/cdn.vox-cdn.com/uploads/chorus_image/image/56558997/631504400.0.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            2,
+            5,
+            2,
+            3,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Jim",
+        photo: "https://www.outkickthecoverage.com/wp-content/uploads/harbaugh.jpg",
+        scores: [
+            3,
+            4,
+            5,
+            1,
+            3,
+            2,
+            4,
+            5,
+            3,
+            2
+        ]
+    },
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,45 @@
 var path = require("path");
 var friendsArray = require("../data/friends.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsArray);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
 
         var newUser = req.body;
 
         newUser.scores = newUser["scores[]"];
 
-        delete newUser ["scores[]"];
+        delete newUser["scores[]"];
 
         for (var i = 0; i < newUser.scores.length; i++) {
             newUser.scores[i] = parseInt(newUser.scores[i])
         };
 
+        var match = friendsArray[0];
+
+        var friendScore = 0;
+
+        for (var i = 0; i < newUser.scores.length; i++) {
+            friendScore += math.abs(newUser.scores[i] - match.scores[i]);
+        };
+
+        for (var i = 1; i < friendsArray.length; i++) {
+            var currentScore = 0;
+            for (var j = 0; j < newUser.scores.length; j++) {
+                currentScore += math.abs(newUser.scores[j] - friendsArray[i].scores[j]);
+            };
+            if (currentScore < friendScore) {
+                match = friendsArray[i];
+                friendScore = currentScore;
+            };
+        };
+
         friendsArray.push(newUser);
-        
+
         res.json(newUser);
     });
 
